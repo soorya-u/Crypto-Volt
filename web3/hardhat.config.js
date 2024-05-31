d1779a87cd8a8a393de0486456f819ee01978e20
Soorya U <sooryau7@gmail.com>
2024-06-01 00:10:28 +0530

    feat: Completed Web3 Contracts
    Signed-off-by: Soorya U <sooryau7@gmail.com>

---
+++ web3/hardhat.config.js
@@ -0,0 +1,51 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  defaultNetwork: "running",
+  running: {
+    url: "http://127.0.0.1:8545/",
+    chainId: 1337,
+  },
+  networks: {
+    zksync_testnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
+      zksync: true,
+    },
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
